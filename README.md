18ffb9928df58609fa7369c2495bfcf790d9ac66
Mario Blažek <blazek.mario@gmail.com>
2020-06-26 13:57:04 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# netgen-layouts-upgrade
+# Netgen Layouts 0.13 to 1.0 upgrade script
+
+This script is not a drop-in replacement for official [upgrade](https://docs.netgen.io/projects/layouts/en/latest/upgrades/upgrade_0130_100.html) procedure. 
+Instead, it tries to simplify the upgrade process by automatizing some upgrade steps.
+
+It is tested with `find` and `sed` commands that are distributed with Mac OS.
+
+The official upgrade procedure is documented [here](https://docs.netgen.io/projects/layouts/en/latest/upgrades/upgrade_0130_100.html).
--- script.sh
+++ script.sh
@@ -1,53 +1,39 @@
 #!/bin/bash
 
+# Replace bundle namespaces
 find app/. -type f -name "*.php" -exec sed -i '' 's/Netgen\\Bundle\\BlockManagerBundle/Netgen\\Bundle\\LayoutsBundle/g' {} +
 find app/. -type f -name "*.php" -exec sed -i '' 's/NetgenBlockManagerBundle/NetgenLayoutsBundle/g' {} +
-
 find app/. -type f -name "*.php" -exec sed -i '' 's/Netgen\\Bundle\\BlockManagerAdminBundle/Netgen\\Bundle\\LayoutsAdminBundle/g' {} +
 find app/. -type f -name "*.php" -exec sed -i '' 's/NetgenBlockManagerAdminBundle/NetgenLayoutsAdminBundle/g' {} +
-
 find app/. -type f -name "*.php" -exec sed -i '' 's/Netgen\\Bundle\\BlockManagerDebugBundle/Netgen\\Bundle\\LayoutsDebugBundle/g' {} +
 find app/. -type f -name "*.php" -exec sed -i '' 's/NetgenBlockManagerDebugBundle/NetgenLayoutsDebugBundle/g' {} +
-
 find app/. -type f -name "*.php" -exec sed -i '' 's/Netgen\\Bundle\\BlockManagerStandardBundle/Netgen\\Bundle\\LayoutsStandardBundle/g' {} +
 find app/. -type f -name "*.php" -exec sed -i '' 's/NetgenBlockManagerStandardBundle/NetgenLayoutsStandardBundle/g' {} +
-
 find app/. -type f -name "*.php" -exec sed -i '' 's/Netgen\\Bundle\\BlockManagerUIBundle/Netgen\\Bundle\\LayoutsUIBundle/g' {} +
 find app/. -type f -name "*.php" -exec sed -i '' 's/NetgenBlockManagerUIBundle/NetgenLayoutsUIBundle/g' {} +
-
 find app/. -type f -name "*.php" -exec sed -i '' 's/Netgen\\Bundle\\EzPublishBlockManagerBundle/Netgen\\Bundle\\LayoutsEzPlatformBundle/g' {} +
 find app/. -type f -name "*.php" -exec sed -i '' 's/NetgenEzPublishBlockManagerBundle/NetgenLayoutsEzPlatformBundle/g' {} +
-
 find app/. -type f -name "*.php" -exec sed -i '' 's/Netgen\\Bundle\\SiteAPIBlockManagerBundle/Netgen\\Bundle\\LayoutsEzPlatformSiteApiBundle/g' {} +
 find app/. -type f -name "*.php" -exec sed -i '' 's/NetgenSiteAPIBlockManagerBundle/NetgenLayoutsEzPlatformSiteApiBundle/g' {} +
 
-
-
-find app/. -type f -name "*.yml" -exec sed -i '' 's/netgen_block_manager/netgen_layouts/g' {} + 
-find src/. -type f -name "*.twig" -exec sed -i '' 's/@ngbm/@nglayouts/g' {} +
+# Update routing
+find app/. -type f -name "*.yml" -exec sed -i '' 's/netgen_block_manager/netgen_layouts/g' {} +
 find src/. -type f -name "*.yml" -exec sed -i '' 's/@ngbm/@nglayouts/g' {} +
 
+find src/. -type f -name "*.twig" -exec sed -i '' 's/@ngbm/@nglayouts/g' {} +
 find src/. -type f -name "*.twig" -exec sed -i '' 's/ngbm_/nglayouts_/g' {} +
 
 # Netgen Layouts Twig globals
-
 find src/. -type f -name "*.twig" -exec sed -i '' 's/ngbm.layoutTemplate/nglayouts.layoutTemplate/g' {} +
 find src/. -type f -name "*.twig" -exec sed -i '' 's/ngbm.pageLayoutTemplate/nglayouts.pageLayoutTemplate/g' {} +
 find src/. -type f -name "*.twig" -exec sed -i '' 's/ngbm.debug/nglayouts.debug/g' {} +
-find src/. -type f -name "*.twig" -exec sed -i '' 's/ngbm.layout/nglayouts.layout/g' {} + 
+find src/. -type f -name "*.twig" -exec sed -i '' 's/ngbm.layout/nglayouts.layout/g' {} +
 
 # Semantic configuration roots have been renamed
-
 find src/. -type f -name "*.yml" -exec sed -i '' 's/netgen_block_manager/netgen_layouts/g' {} +
 find app/. -type f -name "*.yml" -exec sed -i '' 's/netgen_site_api_block_manager/netgen_layouts_ez_platform_site_api/g' {} +
 
-# Find translation files to rename
-
-find src/. -type f -name "ngbm.*.yml"
-find app/. -type f -name "ngbm.*.yml"
-
 # Symfony dependency injection tags:
-
 find src/. -type f -name "*.yml" -exec sed -i '' 's/netgen_block_manager.block.block_definition_handler/netgen_layouts.block_definition_handler/g' {} +
 find src/. -type f -name "*.yml" -exec sed -i '' 's/netgen_block_manager.block.block_definition_handler.plugin/netgen_layouts.block_definition_handler.plugin/g' {} +
 find src/. -type f -name "*.yml" -exec sed -i '' 's/netgen_block_manager.collection.query_type_handler/netgen_layouts.query_type_handler/g' {} +
@@ -63,31 +49,26 @@ find src/. -type f -name "*.yml" -exec sed -i '' 's/netgen_block_manager.item.va
 find src/. -type f -name "*.yml" -exec sed -i '' 's/netgen_block_manager.item.value_loader/netgen_layouts.cms_value_loader/g' {} +
 find src/. -type f -name "*.yml" -exec sed -i '' 's/netgen_block_manager.item.value_url_generator/netgen_layouts.cms_value_url_generator/g' {} +
 
-Roles
-
+# Roles
 find app/. -type f -name "*.yml" -exec sed -i '' 's/ROLE_NGBM_/ROLE_NGLAYOUTS_/g' {} +
 find src/. -type f -name "*.yml" -exec sed -i '' 's/ROLE_NGBM_/ROLE_NGLAYOUTS_/g' {} +
 
-
+# Event classes
 find src/. -type f -name "*.php" -exec sed -i '' 's/BlockManagerEvents/LayoutsEvents/g' {} +
 find src/. -type f -name "*.php" -exec sed -i '' 's/BlockManagerAdminEvents/LayoutsAdminEvents/g' {} +
 
-
-
+# Layouts template references
 find src/. -type f -name "*.yml" -exec sed -i '' 's/@NetgenBlockManager\/api/@NetgenLayoutsAdmin\/app/g' {} +
 find src/. -type f -name "*.yml" -exec sed -i '' 's/@NetgenBlockManagerStandard\/api/@NetgenLayoutsStandard\/app/g' {} +
 
-
-Watch for breadcrumb-wrapper
-
+# Watch for breadcrumb-wrapper
 find src/. -type f -name "*.twig" -exec sed -i '' 's/bm-/ngl-/g' {} +
 find src/. -type f -name "*.scss" -exec sed -i '' 's/bm-/ngl-/g' {} +
 
-
 find app/. -type f -name "*.yml" -exec sed -i '' 's/ngbm_api_/nglayouts_app_api_/g' {} +
-
-
 find src/. -type f -name "*.twig" -exec sed -i '' 's/nglayouts_render_item/nglayouts_render_result/g' {} +
 
-
-
+# Find translation files to rename
+echo "Translation files can't be renamed automatically, here is a list what needs to be changed:"
+find src/. -type f -name "ngbm.*.yml"
+find app/. -type f -name "ngbm.*.yml"
